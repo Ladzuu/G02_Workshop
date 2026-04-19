73e371174b9805a528f92a7ae55ff53ba37aea7d
Ladzuu <lucaslabiouse1243@gmail.com>
2026-04-19 16:26:29 +0200

    Swap Extérieur -> Intérieur au clic

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,35 @@ import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 const body = document.querySelector('body');
 
-// Chemins vers nos modèles
+// Chargement des modèles
 const models = {
-    exterieur: '/models/outside/G2_exterieur_baked.gltf'
+    exterieur: '/models/outside/G2_exterieur_baked.gltf',
+    interieur: '/models/inside/G2_SM_piece.gltf',
+    avatar1: '/models/inside/G2_DM_avatar_1.gltf',
+    avatar2: '/models/inside/G2_DM_avatar_2.gltf',
+    avatar3: '/models/inside/G2_DM_avatar_3.gltf',
+    avatar4: '/models/inside/G2_DM_avatar_4.gltf',
+    cadre: '/models/inside/G2_DM_cadre.gltf',
+    cartons1: '/models/inside/G2_DM_cartons_1.gltf',
+    cartons2: '/models/inside/G2_DM_cartons_2.gltf',
+    cartons3: '/models/inside/G2_DM_cartons_3.gltf',
+    cartons4: '/models/inside/G2_DM_cartons_4.gltf',
+    cartons5: '/models/inside/G2_DM_cartons_5.gltf',
+    cartons6: '/models/inside/G2_DM_cartons_6.gltf',
+    cartons7: '/models/inside/G2_DM_cartons_7.gltf',
+    lampeFusee: '/models/inside/G2_DM_lampe_fusee.gltf',
+    lecteurCd: '/models/inside/G2_DM_lecteur_cd.gltf',
+    petitTrain: '/models/inside/G2_DM_petit_train.gltf',
+    skate: '/models/inside/G2_DM_skate.gltf',
+    armoirCarre: '/models/inside/G2_SM_armoir_carre_combined.gltf',
+    armoir: '/models/inside/G2_SM_armoir.gltf',
+    bureau: '/models/inside/G2_SM_bureau_combined.gltf',
+    chaiseCoffre: '/models/inside/G2_SM_chaise+coffre_combined.gltf',
+    chevetEtager: '/models/inside/G2_SM_chevetetager_muralcorbeille_combined.gltf',
+    fenetrePlafonier: '/models/inside/G2_SM_fenetreplafonier_combined.gltf',
+    lit: '/models/inside/G2_SM_lit.gltf',
+    porteTapispanier: '/models/inside/G2_SM_portetapispanier_combined.gltf',
+    posterBall: '/models/inside/G2_SM_posterball_combined.gltf'
 };
 
 // LOADING
@@ -23,7 +49,6 @@ gltfLoader.setDRACOLoader(dracoLoader);
 for( const key in models ){
   models[ key ] = await gltfLoader.loadAsync( models[ key ] );
 }
-
 
 // Enlève le loader
 body.classList.remove( 'loading' );
@@ -50,19 +75,7 @@ class Viewer {
         this.setRenderer(options);
     }
 
-
     animate( timestamp ) {
-    // const deltaTime = (timestamp - previousTime) * 0.001;
-    // previousTime = timestamp;
-    //     this.mixer.update( deltaTime );
-    //     this.mixer2.update( deltaTime );
-    //     this.render();
-    //     window.requestAnimationFrame(( timestamp) => {
-    //         this.animate(timestamp);
-    //     })
-    }
-
-    animate2( timestamp ) {
 
         const deltaTime = (timestamp - previousTime) / 1000;
         previousTime = timestamp;
@@ -72,7 +85,7 @@ class Viewer {
         this.render();
 
         window.requestAnimationFrame( ( timestamp ) => {
-            this.animate2( timestamp );
+            this.animate( timestamp );
         } );
     }
 
@@ -81,6 +94,7 @@ class Viewer {
         const model = models.exterieur.scene;
         model.rotation.y = THREE.MathUtils.degToRad(270);
         this.scene.add( model );
+        this.currentModel = model;
 
         const ambientLight = new THREE.AmbientLight( 'white', 1 );
         this.scene.add( ambientLight );
@@ -89,10 +103,45 @@ class Viewer {
         this.mixer = new THREE.AnimationMixer( this.scene );
 
         window.requestAnimationFrame( ( timestamp ) => {
-            this.animate2( timestamp );
+            this.animate( timestamp );
         } );
 
+        // Bakcground du canvas
+        this.scene.background = new THREE.Color( 0x1e1e1e );
+
         // Demander un rendu
+        this.render();
+    }
+
+    switchInterior() {
+        this.scene.remove(this.currentModel);
+
+        // Modèles pièce intérieur
+        this.interiorModels = [];
+        const interiorModelItems = [
+            'interieur', 'avatar1', 'avatar2', 'avatar3', 'avatar4', 'cadre', 'cartons1', 'cartons2', 'cartons3', 'cartons4', 'cartons5', 'cartons6', 'cartons7', 'lampeFusee', 'lecteurCd', 'petitTrain', 'skate', 'armoirCarre', 'armoir', 'bureau', 'chaiseCoffre', 'chevetEtager', 'fenetrePlafonier', 'lit', 'porteTapispanier', 'posterBall'
+        ];
+
+        interiorModelItems.forEach(item => {
+            const model = models[item].scene;
+            model.rotation.y = THREE.MathUtils.degToRad(270);
+            this.scene.add(model);
+            this.interiorModels.push(model);
+        });
+
+        this.camera.position.set(1, 0, 1);
+
+        this.controls.enablePan = true;
+        this.controls.minDistance = 1;
+        this.controls.maxDistance = 1;
+        this.controls.minPolarAngle = 0;
+        this.controls.maxPolarAngle = Math.PI;
+        this.controls.minAzimuthAngle = -Infinity;
+        this.controls.maxAzimuthAngle = Infinity;
+
+        // Mettre à jour le mixer pour la nouvelle scène
+        this.mixer = new THREE.AnimationMixer(this.scene);
+
         this.render();
     }
 
@@ -115,6 +164,7 @@ class Viewer {
 
     setRenderer(options = {}) {
         this.renderer = new THREE.WebGLRenderer(options);
+        this.canvas.style.filter = 'grayscale(100%)';
 
         // Crée notre caméra
         // PerspectiveCamera( fov, aspect-ratio, near, far )
@@ -131,7 +181,7 @@ class Viewer {
         this.camera.position.y = 5;
         this.camera.position.z = 10;
 
-        // Orbit Control Settings
+        // Orbit Controls
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
         this.controls.enableDamping = true;
         this.controls.enablePan = false;
@@ -188,4 +238,11 @@ const myViewer = new Viewer(threejsOptions);
 // gère les changements de tailles
 window.addEventListener("resize", () => {
     myViewer.resize();
+});
+
+const btnEntry = document.querySelector(".btnEntry");
+btnEntry.addEventListener("click", () => {
+    myViewer.switchInterior();
+
+    btnEntry.remove();
 });
